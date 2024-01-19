c429d124e34adab0871bc9766846d9ab8ae05bf8
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-19 18:58:02 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        ListBox1 = New ListBox()
+        GroupBox1 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(70, 63)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(69, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Word:"
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = Color.White
+        Label2.Location = New Point(37, 268)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(421, 30)
+        Label2.TabIndex = 1
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(70, 147)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(373, 94)
+        ListBox1.TabIndex = 2
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(26, 315)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 100)
+        GroupBox1.TabIndex = 3
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Case"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(11, 61)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(72, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "To Lower"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(11, 26)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(72, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "To Upper"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton4)
+        GroupBox2.Controls.Add(RadioButton3)
+        GroupBox2.Location = New Point(272, 315)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(200, 100)
+        GroupBox2.TabIndex = 4
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Colour"
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(19, 61)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(48, 19)
+        RadioButton4.TabIndex = 1
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Blue"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(19, 26)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(59, 19)
+        RadioButton3.TabIndex = 0
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Purple"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(70, 81)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(358, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(195, 118)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(ListBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text.Trim)
+        Label2.Text = TextBox1.Text.Trim
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim Word As String = ListBox1.Text
+        If ListBox1.Items.Add(TextBox1.Text.Trim) = True Then
+            MessageBox.Show("Item already exists")
+        Else ListBox1.Items.Add(Word)
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        Label2.Text = Label2.Text.ToUpper()
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        Label1.Text = Label1.Text.ToLower()
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        Label2.ForeColor = Color.Purple
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        Label2.ForeColor = Color.Blue
+    End Sub
+
+
+End Class
